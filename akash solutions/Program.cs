55f500238d1997aad82706347c04800f18ed140d
Vivek Mankar <vivekmankar@usf.edu>
2020-09-28 21:04:24 -0400

    Add project files.

---
+++ akash solutions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace akash_solutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
